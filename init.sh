dcb34c200dfa145f7f5d550d237af17ec9a8cfaf
Basil Veerman <bveerman@uvic.ca>
2015-02-27 14:25:17 -0800

    Fix init script required permissions

--- init.sh
+++ init.sh
@@ -4,13 +4,13 @@ sudo rm -rf cache/maps
 sudo rm -rf cache/data
 sudo rm -rf cache/zips
 
-mkdir -p cache/maps
-mkdir -p cache/data
-mkdir -p cache/zips
+sudo mkdir -p cache/maps
+sudo mkdir -p cache/data
+sudo mkdir -p cache/zips
 
 sudo rm -f logs/access.log
-mkdir -p logs
-touch logs/access.log
+sudo mkdir -p logs
+sudo touch logs/access.log
 
 sudo chown www-data:www-data cache/maps
 sudo chown www-data:www-data cache/data
